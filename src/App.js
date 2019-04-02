d9a67247bf2d42c2a1e60d380d69c00286d119fa
Santiago Fernandez <santiagof@topsystemscorp.com>
2019-04-02 17:07:11 -0300

    first scan

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <Link to="/customers">Customers</Link>
+          <Link to="/customers/3000000">Customer 30000000</Link>
+        </div>
+      </Router>
     );
   }
 }
