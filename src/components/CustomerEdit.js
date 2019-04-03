4adcca62be32e9c76347568019be16906cf36a5e
Santiago Fernandez <santiagof@topsystemscorp.com>
2019-04-03 18:00:42 -0300

    bla

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -1,20 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { reduxForm, Field } from 'redux-form';
+import { setPropsAsInitial } from '../helpers/setPropsAsInitial';
+import CustomersActions from '../components/CustomersActions'
 
-const CustomerEdit = ({name,dni,age}) => {
+const isRequired = value => (
+    !value && "Este campo es requerido"
+);
+
+const isNumber = value => (
+    isNaN(Number(value)) && "El campodebe ser un numero"
+);
+
+const validate = values =>{
+    const error={};
+
+    if(!values.name){
+        error.name= "El campo nombre es requerido";
+    }
+
+    if(!values.dni){
+        error.dni = "El DNI es un campo obligatorio";
+    }
+
+    return error;
+};
+
+const MyField = ({ input, meta, type, label, name }) => (
+    <div>
+        <label htmlFor={name}>{label}</label>
+        <input {...input} type={!type ? "text" : type} />
+        {
+            meta.touched && meta.error && <span>{meta.error}</span>
+        }
+    </div>
+);
+const CustomerEdit = ({ name, dni, age,handleSubmit,submitting,onBack }) => {
     return (
         <div>
             <h2>Edicion del cliente</h2>
-            <h3>{name} / {dni} / {age}</h3>
+            <form onSubmit={handleSubmit}>
+                <Field name="name" component={MyField} type="text" validate={isRequired} label="Nombre"></Field>
+                <Field name="dni" component={MyField} type="text" validate={[isRequired, isNumber]} label="DNI"></Field>
+                <Field name="age" component={MyField} type="number" validate={isNumber} label="Edad"></Field>
+                <CustomersActions>
+                    <button type="submit" disabled={submitting}>Aceptar</button>
+                    <button onClick={onBack}>Cancelar</button>
+                </CustomersActions>
+
+            </form>
         </div>
     );
 };
 
 CustomerEdit.propTypes = {
-    name:PropTypes.string,
-    dni:PropTypes.string,
-    age:PropTypes.number,
-
+    name: PropTypes.string,
+    dni: PropTypes.string,
+    age: PropTypes.number,
+    onBack: PropTypes.func.isRequired,
 };
 
-export default CustomerEdit;
+const CustomerEditForm = reduxForm({ form: 'CustomerEdit', validate})(CustomerEdit);
+export default setPropsAsInitial(CustomerEditForm);
--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import {Route} from 'react-router-dom';
+import {Route, withRouter} from 'react-router-dom';
 import AppFrame from './../components/AppFrame';
 import { getCustomerByDni } from '../selectors/customers';
 import CustomerEdit from './../components/CustomerEdit';
 import CustomerData from './../components/CustomerData';
+import {fetchCustomers} from './../actions/fetchCustomers';
 
 class CustomerContainer extends Component {
     //<p>Datos del cliente {this.props.customer.name}</p>
 
+    componentDidMount(){
+        if(!this.props.customer){
+            this.props.fetchCustomers();
+        }
+    }
+
+    handleSubmit = values =>{
+        
+    }
+
+    handleOnBack = () =>{
+        this.props.history.goBack();
+    }
+    //seguir en 16/13
     renderBody = () => (
         <Route path="/customers/:dni/edit" children={
             ({match}) => {
                 const CustomerControl = match ? CustomerEdit : CustomerData;
-                return <CustomerControl {...this.props.customer}></CustomerControl>;
+                return <CustomerControl {...this.props.customer} onSubmit={this.handleSubmit} onBack={this.handleOnBack}/>;
             }
         }/>
     )
@@ -33,10 +48,13 @@ class CustomerContainer extends Component {
 CustomerContainer.propTypes = {
     dni: PropTypes.string.isRequired,
     customer: PropTypes.object.isRequired,
+    fetchCustomers: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state, props) => ({
     customer: getCustomerByDni(state,props)
 });
 
-export default connect(mapStateToProps, null)(CustomerContainer);
+export default withRouter( connect(mapStateToProps, {
+    fetchCustomers
+})(CustomerContainer));
